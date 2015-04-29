c175b288e73069d73e817ba39568555c1a07f3f2
Kousuke Nagatsuka <koron393@gmail.com>
2015-04-30 02:57:59 +0900

    Fix GCC-4.9 error and fix miss merge

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -2966,8 +2966,6 @@ static void put_event(struct perf_event *event)
 	    event->attr.constraint_duplicate)
 		event->state = PERF_EVENT_STATE_ACTIVE;
 
-	file->private_data = NULL;
-
 	rcu_read_lock();
 	owner = ACCESS_ONCE(event->owner);
 	/*
